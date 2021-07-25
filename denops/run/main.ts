821fa7a2fc67ae57f92a879f489734ea8e95bf4a
skanehira <sho19921005@gmail.com>
2021-07-25 12:00:56 +0900

    make init

--- denops/run/main.ts
+++ denops/run/main.ts
@@ -2,13 +2,13 @@ import { Denops, isString } from "./deps.ts";
 
 export async function main(denops: Denops): Promise<void> {
   await denops.cmd(
-    `command! -nargs=1 Hello call denops#notify("${denops.name}", "hello", [<f-args>])`,
+    `command! -nargs=1 Run call denops#notify("${denops.name}", "run", [<f-args>])`,
   );
 
   denops.dispatcher = {
-    async hello(arg: unknown): Promise<void> {
-      if (isString(arg)) {
-        console.log("hello", arg);
+    async run(args: unknown): Promise<void> {
+      if (isString(args)) {
+        console.log("hello", args);
       }
       await Promise.resolve();
     },
